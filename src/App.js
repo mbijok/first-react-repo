325b98475f6a92af99dd07819ed31c759e52b626
Mariusz Bijok <mbijok@outlook.com>
2021-05-11 10:03:19 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="bg-green-200 max-w-max p-10 text-indigo-600 font-black m-auto">
+      <h1 className="bg-blue-200 max-w-max p-10 text-indigo-600 font-black m-auto">
         Hello world!
       </h1>
     </>
